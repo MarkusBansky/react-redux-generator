12e09c8dfa5521cc24d311e375370844c29e098b
Markiian Benovskyi <benovskym@gmail.com>
2019-11-14 21:31:55 +0000

    Added more useful packages

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var chalk = require("chalk");
+var boxen = require("boxen");
 var log = console.log;
+var greetingMessage = chalk.red('React') + ' ' + chalk.green('Redux') + ' ' + chalk.blue('Generator');
 // Combine styled and normal strings
-log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+log(boxen(greetingMessage, { padding: 1, margin: 1, borderStyle: 'double' }));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
 import * as chalk from 'chalk';
+import * as boxen from 'boxen';
 const log = console.log;
 
-// Combine styled and normal strings
-log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+// Log greeting
+const greetingMessage = chalk.red('R') + 'eact ' + chalk.green('R') + 'edux ' + chalk.blue('G') + 'enerator';
+log(boxen(greetingMessage, {padding: 1, margin: 1, borderStyle: 'double' as any}));
+
+// Create the generator class instance:
